549f08593cc039840c95a3ee8189c4a091b382d4
AdmiralKeso <cas.hagenaar@gmail.com>
2026-06-22 23:49:57 +0200

    Remove name from public fillable

--- game-project/app/Models/User.php
+++ game-project/app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
     ];
@@ -42,7 +42,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'password' => 'hashed', //Password hash using laravels built in hashing mechanism
         ];
     }
 }
